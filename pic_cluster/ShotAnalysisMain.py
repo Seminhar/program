1191efcd28ceb7b4f52616437e7f6385c5267eb5
seminhar <1215356288@qq.com>
2018-06-08 23:09:03 +0800

    push

--- pic_cluster/ShotAnalysisMain.py
+++ pic_cluster/ShotAnalysisMain.py
@@ -76,13 +76,16 @@ def shot_screenTxtCluster():
                 if(p1_index<=p1_l and frame>0):
                     print "//////////////自增索引值1",p1_index
                     p1_deal=re.image_deal(Image.open(add_pic + str(p1_index)+ '.jpg'))
-                    text1=re.textExtract(p1_deal).strip()
+                    text1=re.textExtract(p1_deal).strip().replace("  ","")
+                    print "t1",text1
                     p1_index += frame 
                 elif(frame<0):
                     print "//////////////逆序自增索引值1",p1_index
                     p1_deal=re.image_deal(Image.open(add_pic + str(p1_index)+ '.jpg'))
-                    text1=re.textExtract(p1_deal).strip()
-                    p1_index += frame 
+                    text1=re.textExtract(p1_deal).strip().replace("  ","")
+                    print "t1",text1
+                    p1_index += frame
+                   
                 else:
                     p1_index = p1_p
                     p2_index = p2_p
@@ -92,9 +95,12 @@ def shot_screenTxtCluster():
                 if(p2_index<=p2_l):
                     print "//////////////自增索引值2",p2_index
                     p2_deal=re.image_deal(Image.open(add_pic + str(p2_index)+ '.jpg'))          
-                    text2=re.textExtract(p2_deal).strip()
-                    p2_index += frame
-
+                    text2=re.textExtract(p2_deal).strip().replace("  ","")
+                    print "t2 ",text2
+                    if(frame<0):
+                        p2_index = p2_index + (0-frame)                       
+                    else:
+                        p2_index += frame                  
                 else:
                     p1_index = p1_p
                     p2_index = p2_p
@@ -105,60 +111,72 @@ def shot_screenTxtCluster():
     # =============================================================================
                 if(len(text1)>3 and len(text2)>3):        
                     ratio=re.TextSimilarity(text1.strip(),text2.strip())
-                    if(ratio<0.5):
+                    print "第一次比较得到的相似度",ratio
+                    if(ratio<0.6 and ratio>0.25):
                        print "二次检测！！！！！"
                        p1_deal1,p1_deal2=re.image_deal2(Image.open(add_pic + str(p1_index)+ '.jpg'))
-                       text1=re.textExtract(p1_deal1).strip()
-                       text2=re.textExtract(p1_deal2).strip()
+                       text1=re.textExtract(p1_deal1).strip().replace("  ","")
+                       text2=re.textExtract(p1_deal2).strip().replace("  ","")
                        
                        p2_deal1,p2_deal2=re.image_deal2(Image.open(add_pic + str(p2_index)+ '.jpg'))
-                       text3=re.textExtract(p1_deal1).strip()
-                       text4=re.textExtract(p1_deal2).strip()
+                       text3=re.textExtract(p2_deal1).strip().replace("  ","")
+                       text4=re.textExtract(p2_deal2).strip().replace("  ","")
                        print "二次检测文本TTTTTTTT",text1.strip(),"___",text2.strip(),"___",text3.strip(),"____",text4.strip()
-                       if(len(text1.strip())>3 and len(text3.strip())>3):
-                           ratio1=re.TextSimilarity(text1.strip(),text3.strip())
-                           print "1和3比较",len(text1.strip()),len(text3.strip()),ratio1
-                       elif(len(text2.strip())>3 and len(text4.strip())>3):
-                           ratio2=re.TextSimilarity(text2.strip(),text4.strip())
-                           print "2和4比较",len(text2.strip()),len(text4.strip()),ratio2
-                       elif(len(text1.strip())>3 and len(text4.strip())>3):
-                           ratio3=re.TextSimilarity(text1.strip(),text4.strip())
+                       if(len(text1)>3 and len(text3)>3):
+                           ratio1=re.TextSimilarity(text1,text3)
+                           print "1和3比较",len(text1),len(text3),ratio1
+                       elif(len(text2)>3 and len(text4)>3):
+                           ratio2=re.TextSimilarity(text2,text4)
+                           print "2和4比较",len(text2),len(text4),ratio2
+                       elif(len(text1)>3 and len(text4)>3):
+                           ratio3=re.TextSimilarity(text1,text4)
                            print "1和4比较",len(text1.strip()),len(text4.strip()),ratio3
-                       elif(len(text2.strip())>3 and len(text3.strip())>3):
-                           ratio4=re.TextSimilarity(text2.strip(),text3.strip())
-                           print "2和3比较",len(text2.strip()),len(text3.strip()),ratio4
+                       elif(len(text2)>3 and len(text3)>3):
+                           ratio4=re.TextSimilarity(text2,text3)
+                           print "2和3比较",len(text2),len(text3),ratio4
                        else:
-                           ratio1=0
+                           ratio1=0 
                            ratio2=0
                            ratio3=0
                            ratio4=0
                        if(ratio1>0.8 or ratio2>0.8 or ratio3>0.8 or ratio4>0.8):
-                           print "二次检测相似"
+                           print "二次检测判定相似！！！！！"
                            ratio=1
                 else:
                     ratio=0
                     print "文本过短或者无文本，相似度置零"
-                if(ratio>0.5 and ratio!=1):            
+# =============================================================================
+#                     
+# =============================================================================
+                if(ratio>0.55 and ratio <0.85 and ratio!=1):            
                     count+=1
                 elif(ratio==-1):
                     count_null+=1 
                     print "相似度为-1的个数",count_null
-                elif(ratio==0 or ratio<=0.5):
+                elif(ratio==0 or ratio<=0.55):
                     count_0+=1 
                     print "相似度为0 或者小于0.5的个数",count_0
-                elif(ratio==1):
+                elif(ratio==1 or ratio>=0.85):
                     count_1 +=1
                     print "相似度为1的个数",count_1
                     
     # =============================================================================
     #数组操作       
     # =============================================================================
-                if(new_order_L>2 and (count_1>=5 or count>=10)):           
+                if(new_order_L>2 and (count_1>2 or count>=10)):           
                     if(new_order[new_order_L-1]==p1_l):
                         new_order[new_order_L-1]=p2_l
                         total=new_order.tolist()
                         print  "%%%%%%%%%%%%前一组第一个值",new_order[new_order_L-2]
                         print  "%%%%%%%%%%%%新一组最后的值",p2_l
+                        print   new_order
+                    elif(i>0 and new_order[new_order_L-3]==p1_l):
+                        for m in range(4):
+                            new_order.remove(new_order[new_order_L-m])
+                        print "删除数据后",new_order
+                        temp=[p1_p,p2_l]
+                        total=total+temp                         
+                        new_order=np.array(total,int)
                         print   new_order                                 
                     else:
                         temp=[p1_p,p2_l]
@@ -172,7 +190,8 @@ def shot_screenTxtCluster():
                     print "融合的是：",p1_p,p1_l,p2_p,p2_l
                     print "*************融合后更新索引",index_start1,index_end1,index_start2,index_end2
                     break
-                elif(new_order_L<2 and (count_1>=1 or count>=5)):
+                elif(new_order_L<2 and (count_1>2 or count>=10)):
+                       
                     temp=[p1_p,p2_l]
                     total=total+temp                         
                     new_order=np.array(total,int)
@@ -189,19 +208,39 @@ def shot_screenTxtCluster():
                         frame=25*2
                         count_0=0
                         count_null=0
-                        print "再次核对"
+                        p1_index = p1_p
+                        p2_index = p2_p
+                        print "再次核对======================"
                         continue
                     elif(repeat==2):
-                        print "逆序核对"
+                        print "逆序核对======================"
                         p1_index = p1_l
+                        p2_index = p2_p
                         count_0=0
                         count_null=0                        
                         frame=-10                      
                         continue
-                    elif(repeat==3):                    
+                    elif(repeat==3):
+                        print "向后探测一步"
+                        print "1前",p1_p
+                        print "1后",p1_l
+                        print "1前",p2_p
+                        print "1后",p2_l
+                        count_0=0
+                        count_null=0
+                        if(i>2):
+                            p1_p=order_FSX_R[index_start1-2]
+                            p1_l=order_FSX_R[index_end1-2]
+                            p1_index = p1_p        
+                        frame=10
+                        continue
+                    elif(repeat==4):                    
                         temp=[p1_p,p1_l]
-                        temp_=[p2_p,p2_l]
+                        temp_=[p2_p,p2_l] 
                         if(i>0 and new_order[new_order_L-1]==p1_l):                    
+                            total=total+temp_
+                            print "不融合的是：",temp_
+                        elif(i>0 and new_order[new_order_L-3]==p1_l):
                             total=total+temp_
                             print "不融合的是：",temp_
                         else:
